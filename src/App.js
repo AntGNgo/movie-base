1a53badfdb88c2757603b232d7a92012d5ef6b78
Anthony Ngo <anthony.g.ngo@gmail.com>
2020-06-09 01:27:26 -0700

    Changed API and added posters

--- src/App.js
+++ src/App.js
@@ -10,24 +10,47 @@ class App extends Component {
     this.state = {
       movieTitle : null,
       movieYear : null,
-      moviePlot: null
+      moviePlot: null,
+      moviePoster: null
     }
   }
 
-
-  getMovieData = (search) => {
+//Poster Data
+  getMovieData = (keyword) => {
     let that = this
-    let query = search.search.split(' ').join('+')
-    fetch('http://www.omdbapi.com/?apikey=797a1e18&t=' + query)
-      .then(res => res.json())
+    let configData = null
+    let baseImageURL = null
+    
+
+    const posterConfig = "https://api.themoviedb.org/3/configuration?api_key=0017e637b5175be645272456f9607e35"
+    fetch(posterConfig)
+      .then(resp => resp.json())
       .then(data => {
-        that.setState({
-          movieTitle: data.Title,
-          movieYear: data.Year,
-          moviePlot: data.Plot
-        })
+        baseImageURL = data.images.secure_base_url
+        configData = data.images
+        console.log('config', data)
+        console.log(baseImageURL)
+        runSearch(keyword)
       })
-  }
+  
+  
+    const runSearch = (keyword) => {
+      let query = keyword.search.split(' ').join('+')
+      fetch('https://api.themoviedb.org/3/search/movie?api_key=0017e637b5175be645272456f9607e35&language=en-US&query=' + query)
+        .then(resp => resp.json())
+        .then(data => {
+          that.setState({
+            movieTitle: data.results[0].title,
+            movieYear: data.results[0].release_date,
+            moviePlot:  data.results[0].overview,
+            moviePoster: baseImageURL +  'w342' + data.results[0].poster_path        
+          })
+        })
+    }  
+  
+    }
+
+
 
   render() {
     return(
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import '../styles/moviecard.css'
+
 
 const MovieCard = ({ movie }) => {
     return(
-        <div>
+        <div className='movie-card'>
             <h1>{movie.movieTitle}</h1>
             <h2>{movie.movieYear}</h2>
+            <img src={movie.moviePoster} alt=""/>
             <h3>{movie.moviePlot}</h3>
+
         </div>
     )
 }
