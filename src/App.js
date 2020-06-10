e99a2df11197b32bbe8f86bc2e7918f78e6ddcc3
Anthony Ngo <anthony.g.ngo@gmail.com>
2020-06-09 23:31:17 -0700

    WIP add search list

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 import './styles/App.css';
 import Nav from './components/Nav'
 import MovieCard from './components/MovieCard'
-
+import SearchResults from './components/SearchResults'
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
+      searchList : null,
       movieTitle : null,
       movieYear : null,
       moviePlot: null,
@@ -37,7 +38,9 @@ class App extends Component {
       fetch('https://api.themoviedb.org/3/search/movie?api_key=0017e637b5175be645272456f9607e35&language=en-US&query=' + query)
         .then(resp => resp.json())
         .then(data => {
+          console.log(data)
           that.setState({
+            searchList:  data.results,
             movieTitle: data.results[0].title,
             movieYear: data.results[0].release_date.slice(0,4),
             moviePlot:  data.results[0].overview,
@@ -45,16 +48,16 @@ class App extends Component {
           })
         })
     }  
-  
     }
 
 
-
   render() {
+    console.log(this.state.searchList)
     return(
       <div>
         <Nav getMovieData={this.getMovieData}/>
         <MovieCard movie={this.state} />
+        <SearchResults searchResults={this.state.searchList} />
       </div>
     )
   }
---
+++ src/components/SearchResults.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SearchResults = ({ searchResults }) => {
+    return(
+        <div>he</div>
+    )
+}
+
+export default SearchResults
