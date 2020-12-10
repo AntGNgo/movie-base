b759cb97f3c6ae1f3597edb781fd5feeecee1219
AntGNgo <anthony.g.ngo@gmail.com>
2020-12-10 15:29:04 -0800

    WIP Movie page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React, { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import axios from 'axios'
 import Nav from './components/Nav'
 import MovieCard from './components/MovieCard/MovieCard'
-import './app.css'
+import './styles/app.css'
 
 const App = () => {
     const [searchResults, setSearchResults] = useState([])
+
+    useEffect(() => {
+        const baseURL = 'http://image.tmdb.org/t/p/w342'
+        axios.get('https://api.themoviedb.org/3/discover/movie/?api_key=0017e637b5175be645272456f9607e35&language=en-US')
+            .then(response => {
+                console.log(response.data)
+                setSearchResults(response.data.results.map((movie) => {
+                    return {
+                        id: movie.id,
+                        title: movie.title,
+                        year: movie.release_date,
+                        poster: baseURL + movie.poster_path
+                    }
+                }))
+            })
+    }, [])
     
     const runSearch = (search) => {
         const baseURL = 'http://image.tmdb.org/t/p/w342'
